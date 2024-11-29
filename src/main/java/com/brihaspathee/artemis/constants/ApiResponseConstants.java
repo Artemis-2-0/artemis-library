948aaa7db11e029efa491af9cd128d382d229c2c
vbalaji215-dev <vbalaji215@outlook.com>
2024-11-28 21:46:45 -0500

    Updated constants

--- src/main/java/com/brihaspathee/artemis/constants/ApiResponseConstants.java
+++ src/main/java/com/brihaspathee/artemis/constants/ApiResponseConstants.java
@@ -16,6 +16,6 @@ public class ApiResponseConstants {
     public static final String API_SUCCESS = "10000";
     public static final String API_FAIL = "11000";
     public static final String SUCCESS_REASON = "API Call was very successful";
-    public static final String FAILURE_REASON = "API Call was not successful";
+    public static final String FAILURE_REASON = "API Call was not very successful";
     public static final String TRADING_PARTNER_API = "TRADING_PARTNER_API";
 }
